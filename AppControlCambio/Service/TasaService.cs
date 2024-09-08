c64921e03f8ffa6b17382901fc4b8af3069ae18d
wilderson victor contreras mujica <wilderson_13@hotmail.com>
2024-09-08 03:15:45 -0400

    Cambio para Compartir tasas

--- AppControlCambio/Service/TasaService.cs
+++ AppControlCambio/Service/TasaService.cs
@@ -9,23 +9,19 @@ using System.Threading.Tasks;
 
 namespace AppControlCambio.Service
 {
-    public class TasaService : ITasaService
+    public class TasaService(HttpClient httpClient) : ITasaService
     {
-        private readonly HttpClient _httpClient;
+        private readonly HttpClient _httpClient = httpClient;
 
-        public TasaService(HttpClient httpClient )
-        {
-            _httpClient = httpClient;
-        }
         public async Task<List<string>> GetCountries()
         {
-            List<string> listCountry = new ();
+            List<string> listCountry = [];
             var response = await _httpClient.GetAsync("Tasa/Paises");
             var responseBody = await response.Content.ReadAsStringAsync();
 
             var lista = JsonSerializer.Deserialize<List<Pais>>(responseBody); // TODO : Modificar cuando el api solo envia la lista nombre de paises
 
-            if (lista.Any())
+            if (lista.Count != 0)
             {
                 foreach (var item in lista)
                 {
@@ -38,11 +34,11 @@ namespace AppControlCambio.Service
 
         public async Task<List<ResponseTasas>> GetTasas(string pais,int porcentaje)
         {
-            List<ResponseTasas> ListaTasas = new ();
+            List<ResponseTasas> ListaTasas = [];
             var response = await _httpClient.GetAsync($"Tasa/Tasas/{pais}?por={porcentaje}");
             var responseBody = await response.Content.ReadAsStringAsync();
             var lista = JsonSerializer.Deserialize<List<ResponseTasas>>(responseBody);
-            if (lista.Any())
+            if (lista.Count > 0)
             {
                 foreach (var item in lista)
                 {
--- AppControlCambio/ViewModel/TasasViewModel.cs
+++ AppControlCambio/ViewModel/TasasViewModel.cs
@@ -1,75 +1,82 @@
 ﻿
+
 using AppControlCambio.Pages;
 using AppControlCambio.Service;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using System.Reflection;
+
 using Shared;
-using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Text.Json;
+using Microsoft.Maui.Graphics.Platform;
 using System.Threading.Tasks;
+using Microsoft.Maui.ApplicationModel;
+
+
+
+
+
 
 namespace AppControlCambio.ViewModel
 {
-    public partial class TasasViewModel : ObservableObject , IQueryAttributable
+    public partial class TasasViewModel : ObservableObject, IQueryAttributable
     {
+        [ObservableProperty]
+        private IDrawable drawable;
         private readonly ITasaService _tasaService;
         [ObservableProperty]
-        private List<ResponseTasas> listaTasas = new();
+        private List<ResponseTasas> listaTasas = [];
         [ObservableProperty]
         private string titulo;
         [ObservableProperty]
         private bool activityIndicator = false;
-
         private int selectedPorcentaje = 0;
-
         public int SelectedPorcentaje
         {
-            get {  return selectedPorcentaje; } 
-            
+            get { return selectedPorcentaje; }
+
             set
             {
                 if (selectedPorcentaje != value)
                 {
-                    
+
                     selectedPorcentaje = value;
                     OnPropertyChanged(nameof(SelectedPorcentaje));
                     MainThread.BeginInvokeOnMainThread(new Action(async () => await GetTasas()));
-                    
+
                 }
             }
         }
-        
-        
-        private string pais;
-        
-        
-        public  TasasViewModel(ITasaService tasaService )
+        public string pais;
+
+
+        public TasasViewModel(ITasaService tasaService)
         {
             _tasaService = tasaService;
-            MainThread.BeginInvokeOnMainThread(new Action(async () => await GetTasas()));
+            MainThread.BeginInvokeOnMainThread(new Action(async () =>
+            {
+                await GetTasas(); // Esperar a que se llene ListaTasas.
+                Drawable = new CustomDrawable(ListaTasas, pais); // Solo crear después de obtener las tasas.
+                
+            }));
+            
         }
-        
+
         public void ApplyQueryAttributes(IDictionary<string, object> query)
         {
             pais = query["pais"].ToString();
-            Titulo = pais;          
+            Titulo = pais;
         }
 
         public async Task GetTasas()
         {
-            ActivityIndicator = true;        
-            while(pais == null)
+
+            while (pais == null)
             {
                 await Task.Delay(10);
             }
 
             ListaTasas = await _tasaService.GetTasas(pais, selectedPorcentaje);
-            ActivityIndicator = false;
+
         }
 
         [RelayCommand]
@@ -79,5 +86,62 @@ namespace AppControlCambio.ViewModel
             await Shell.Current.GoToAsync(url);
 
         }
+
+        #region Download tasas
+
+        private async void DownloadTasas()
+        {
+            while (ListaTasas.Count == 0) await Task.Delay(10);
+        }
+
+
+
+
+        //Clase que implementa IDrawable para manejar el dibujo
+        public class CustomDrawable(List<ResponseTasas> listTasas, string country) : IDrawable
+        {
+
+            public  void Draw(ICanvas canvas, Microsoft.Maui.Graphics.RectF dirtyRect)
+            {
+
+                Assembly assembly = GetType().GetTypeInfo().Assembly;
+                using Stream stream = assembly.GetManifestResourceStream("AppControlCambio.Resources.Images.changetasas.png");
+                var _image = PlatformImage.FromStream(stream);
+                canvas.DrawImage(_image, 0, 0, dirtyRect.Width, 550);
+                canvas.FontSize = 30;
+                canvas.FontColor = Colors.Black;
+                canvas.Font = Microsoft.Maui.Graphics.Font.DefaultBold;
+                canvas.DrawString($"envia desde\n   {country}",80,150,HorizontalAlignment.Left);
+                using Stream stream1 = assembly.GetManifestResourceStream($"AppControlCambio.Resources.Images.{country}t.png");
+                var _image1 = PlatformImage.FromStream(stream1);
+                canvas.DrawImage(_image1, 80, 200, 150, 150);
+                canvas.FontSize = 24;
+                canvas.Font = Microsoft.Maui.Graphics.Font.DefaultBold;
+
+                var image = 100;
+                var text = 127;
+                foreach (var tasa in listTasas)
+                {
+                    if (tasa.tasa != "No Registrada")
+                    {
+                        using Stream stream2 = assembly.GetManifestResourceStream($"AppControlCambio.Resources.Images.{tasa.pais}t.png");
+
+                        var _image2 = PlatformImage.FromStream(stream2);
+
+                        canvas.DrawImage(_image2, 355, image, 30, 30);
+                        canvas.DrawString(tasa.tasa, 340, text, HorizontalAlignment.Right);// Dibuja el texto sobre la imagen
+                        image += 50;
+                        text += 50;
+                    }
+                   
+                }
+                
+
+            }
+
+
+            
+        }
+        #endregion
     }
 }
